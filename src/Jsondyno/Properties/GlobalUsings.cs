b2ed6f06747ef02a54cfd7c82476cca14d4eee76
Vlad Savik <v.savik@outlook.com>
2024-02-29 16:25:30 +0300

    Better global usings

---
+++ src/Jsondyno/Properties/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using System.Dynamic;
+global using System.Text.Json;
+global using System.Text.Json.Nodes;
+global using System.Text.Json.Serialization;
+global using Jsondyno.Misc;
+global using SR = Jsondyno.Misc.StringResources;
---
+++ src/Jsondyno/Properties/InternalsVisibleTo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Jsondyno.Tests")]
+
+// Required for Moq https://github.com/devlooped/moq/wiki/Quickstart#advanced-features
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
