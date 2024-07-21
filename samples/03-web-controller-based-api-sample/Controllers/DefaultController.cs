a25b54d7a47ec8869113d2ee6beea0feae586d80
Vlad Savik <v.savik@outlook.com>
2024-07-21 14:40:01 +0300

    Fix security issue

--- samples/03-web-controller-based-api-sample/Controllers/DefaultController.cs
+++ samples/03-web-controller-based-api-sample/Controllers/DefaultController.cs
@@ -6,14 +6,6 @@ namespace Jsondyno.ControllerBasedApi.Controllers;
 [ApiController]
 public sealed class DefaultController : ControllerBase
 {
-    private readonly ILogger<DefaultController> _logger;
-
-    public DefaultController(
-        ILogger<DefaultController> logger)
-    {
-        _logger = logger;
-    }
-
     [HttpGet]
     [Route("/")]
     [ApiExplorerSettings(IgnoreApi = true)]
@@ -27,20 +19,10 @@ public sealed class DefaultController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult Submit([FromBody] dynamic? data)
     {
-        try
-        {
-            string? originalObject = data?.ToString();
-            string response = data is not null ?
-                $"First name: {data.FirstName}, Last name: {data.LastName}"
-                : "Input object is null";
+        string response = data is not null ?
+            $"First name: {data.FirstName}, Last name: {data.LastName}"
+            : "Input object is null";
 
-            _logger.LogInformation("Original object: \n{0}\nResult: {1}", originalObject ?? "null", response);
-
-            return Ok(response);
-        }
-        catch (Exception e)
-        {
-            return BadRequest($"Invalid input JSON data: {e}");
-        }
+        return Ok(response);
     }
 }
